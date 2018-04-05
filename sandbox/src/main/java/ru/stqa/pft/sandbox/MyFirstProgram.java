ec4cab6e8c6eb381bf0e7e093f908dde3c914b77
barlog1983 <osipovp@list.ru>
2018-04-05 13:55:47 +0300

    update

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -3,7 +3,13 @@ package ru.stqa.pft.sandbox;
 public class MyFirstProgram {
 
 	public static void main(String[] args) {
+		String somebody = "world";
 		System.out.println("Hello, world!");
+		System.out.println("Hello, " + somebody + "!");
+
+		double l = 8;
+		double s = l * l;
+		System.out.println("Площадь квадрата со стороной " + l + " = " + s);
 	}
 
 }
