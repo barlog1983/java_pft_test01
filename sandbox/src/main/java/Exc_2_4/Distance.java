321953925960705f02ad484a7f5b67132ee513be
barlog1983 <osipovp@list.ru>
2018-04-08 11:55:39 +0300

    Exc 2.1 and 2.4

---
+++ sandbox/src/main/java/Exc_2_4/Distance.java
@@ -0,0 +1,21 @@
+package Exc_2_4;
+
+public class Distance {
+
+  public Point point1; //точка 1.
+  public Point point2; //точка 2.
+  public Distance(Point p1, Point p2) {
+    this.point1 = p1;
+    this.point2 = p2;
+  }
+  public double distance(Point p1, Point p2) {
+return Math.sqrt(Math.pow((p2.p1 - p1.p1), 2) + Math.pow((p2.p2 - p1.p2), 2));
+  }
+  public static void main(String[] args) {
+    Point p1 = new Point(5, 7);
+    Point p2 = new Point(3, 4);
+    Distance D = new Distance(p1, p2);
+    System.out.println("1");
+    System.out.println("Расстояние между точками p1 и p2 = " +  D.distance(p1, p2));
+  }
+}
